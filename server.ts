1d66f0902d48dd913b57b2f19563cb9360d72ead
Eric Harding <eharding@blackedge.com>
2022-11-27 16:55:32 -0500

    Add simple deno fileserver

---
+++ server.ts
@@ -0,0 +1,31 @@
+const server = Deno.listen({ port: 8000 });
+console.log("File server running on http://localhost:8000/");
+
+for await (const conn of server) {
+  handleHttp(conn).catch(console.error);
+}
+
+async function handleHttp(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+
+  for await (const requestEvent of httpConn) {
+    const url = new URL(requestEvent.request.url);
+    const filepath = decodeURIComponent(url.pathname.slice(1));
+
+    // Try opening the file
+    let file;
+    try {
+      console.log(filepath)
+      file = await Deno.open(filepath);
+    } catch {
+      const notFound = new Response("404 not found", { status: 404 });
+      await requestEvent.respondWith(notFound);
+      return;
+    }
+
+    // Use a readableStream so the file doesn't have to be read into memory all at once
+    const readableStream = file.readable;
+    const response = new Response(readableStream);
+    await requestEvent.respondWith(response);
+  }
+}
