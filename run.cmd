ecbe2dcade0429d3e9090f16cf4cfd63defebaf0
Eric Harding <eric@digitalsorcery.net>
2022-11-27 18:55:28 -0500

    Deno is working now? sus

--- run.cmd
+++ run.cmd
@@ -1,3 +1,4 @@
-# deno run --allow-read --allow-net server.ts
+# deno install --allow-read --allow-run --allow-write -f --unstable https://deno.land/x/denon/denon.ts
+# denon run --allow-read --allow-net server.ts
 python3 -m http.server 8000 --bind 127.0.0.1
 
