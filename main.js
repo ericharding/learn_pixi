210531586005bd1fdd30de079a0e5cf650e4589b
Eric Harding <eharding@blackedge.com>
2022-11-27 17:58:25 -0500

    Improve pixi demo

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ bunny.interactive = true;
 bunny.anchor.set(0.5);
 bunny.x = app.screen.width / 2;
 bunny.y = app.screen.height / 2;
+
 bunny.on("pointerdown", (event) => {
   console.log("FAT bunnay!")
   bunny.scale.x *= 1.25;
 });
-bunny.on('click', (event) => { console.log('clicked') });
 
 let elapsed = 0.0;
 app.ticker.add((delta) => {
   elapsed += delta;
-  bunny.x = 100 + Math.sin(elapsed/50) * 100;
+  bunny.x = (app.screen.width/2) + Math.cos(elapsed/50) * 100;
 });
