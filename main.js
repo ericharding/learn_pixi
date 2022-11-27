5dd90b76cc5552d5baa1ee067bb4935d618cda90
Eric Harding <eharding@blackedge.com>
2022-11-27 17:49:15 -0500

    Pixi demo

---
+++ main.js
@@ -0,0 +1,21 @@
+const app = new PIXI.Application({ width: 800, height: 600 });
+document.body.appendChild(app.view);
+
+const bunny = PIXI.Sprite.from("img/bunny.png");
+app.stage.addChild(bunny);
+
+bunny.interactive = true;
+bunny.anchor.set(0.5);
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+bunny.on("pointerdown", (event) => {
+  console.log("FAT bunnay!")
+  bunny.scale.x *= 1.25;
+});
+bunny.on('click', (event) => { console.log('clicked') });
+
+let elapsed = 0.0;
+app.ticker.add((delta) => {
+  elapsed += delta;
+  bunny.x = 100 + Math.sin(elapsed/50) * 100;
+});
