30292384659f246c46de347c673ff10782744ef3
Eric Harding <eharding@blackedge.com>
2023-01-22 11:32:50 -0500

    Import pixi-hotwire template for live reload

--- run.cmd
+++
@@ -1,4 +0,0 @@
-# deno install --allow-read --allow-run --allow-write -f --unstable https://deno.land/x/denon/denon.ts
-# denon run --allow-read --allow-net server.ts
-python3 -m http.server 8000 --bind 127.0.0.1
-
