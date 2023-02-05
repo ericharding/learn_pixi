0a032b9a453e1a356007c99117194d328f6228e0
Eric Harding <eharding@blackedge.com>
2023-02-05 15:53:21 -0500

    bunny run game

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { AnimatedSprite, Application, Assets, Sprite } from "pixi.js";
+import { AnimatedSprite, Application, Assets, Sprite, SimplePlane } from "pixi.js";
 import { loadAssets } from "./assets";
 
 const app = new Application({
@@ -13,28 +13,40 @@ const app = new Application({
 async function gameLoaded() {
   const textures = await Assets.loadBundle("main");
 
+  const background = new SimplePlane(textures.field);
+  background.width = app.screen.width;
+  background.height = app.screen.height;
+  app.stage.addChild(background);
+
   const bunny = Sprite.from(textures.bunny);
-  bunny.anchor.set(0.5)
-  bunny.x = app.screen.width / 2;
-  bunny.y = app.screen.height / 2;
+  bunny.anchor.set(0.5);
+  bunny.x = 0;
+  bunny.y = 400;
   bunny.interactive = true;
   app.stage.addChild(bunny);
 
   const explosion = new AnimatedSprite(textures.boom.animations.boom);
-  explosion.x = bunny.x;
-  explosion.y = bunny.y;
   explosion.anchor.set(0.5);
   explosion.rotation = Math.random() * Math.PI;
   explosion.loop = false;
 
   bunny.on("pointertap", () => {
     app.stage.addChild(explosion);
+    explosion.x = bunny.x;
+    explosion.y = bunny.y;
     explosion.play();
     bunny.visible = false;
+  });
+
+  let speed = 0.5;
+
+  bunny.on('pointermove', () => {
+    speed += 0.1;
+  });
+
+  app.ticker.add((timeDelta) => {
+    bunny.x = (bunny.x + speed * timeDelta) % app.screen.width;
   });
 }
 
 loadAssets(gameLoaded);
-
-
-
